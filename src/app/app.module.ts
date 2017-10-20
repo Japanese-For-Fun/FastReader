a8346abbc4394342857ad49902be8d5b11b844f3
Ильяс Сабешулы <groza01061995@gmail.com>
2017-10-20 22:40:20 +0600

    bugs bugs everywhere

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { UploaderComponent } from './components/main/uploader/uploader.component';
+import { ContentComponent } from './components/main/content/content.component';
+import { UploaderService } from './components/main/uploader/uploader.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    UploaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [UploaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
